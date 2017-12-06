1f524f5ce75567c280d71f05abbbd95b9f7d705d
Ricardo Junio <ricardo.junior.s@hotmail.com>
2017-12-06 13:54:52 -0200

    execução do glpk

---
+++ src/main/java/glpkService.java
@@ -0,0 +1,18 @@
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+
+public class glpkService {
+
+    @GET
+    public String getResult(){
+        return "Api Funciona";
+    }
+
+    @POST
+    public String GLPK (){
+        return "";
+    }
+
+
+}
